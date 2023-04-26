5eb5069ed2e40a320357a156d840639a53b9421b
slashrage <95706367+slashrage@users.noreply.github.com>
2023-04-26 16:11:10 -0500

    definte jokes within an index variable

---
+++ main.js
@@ -0,0 +1,10 @@
+let joke = ["What's the best thing about Switzerland? I don't know, but the flag is a big plus.", 
+        "I invented a new word! Plagiarism!",
+        "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.",
+        "Why do we tell actors to 'break a leg?' Because every play has a cast.",
+        "Helvetica and Times New Roman walk into a bar. 'Get out of here!' shouts the bartender. 'We don't serve your type.'",
+        "Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, 'What's the word on the street?' Once my dog ate all the Scrabble tiles. For days he kept leaving little messages around the house.",
+        "Knock! Knock! Who's there? Control Freak. Con… OK, now you say, “Control Freak who?”",
+        "Hear about the new restaurant called Karma? There's no menu: You get what you deserve.",
+        "A woman in labor suddenly shouted, 'Shouldn't! Wouldn't! Couldn't! Didn't! Can't!' 'Don't worry,' said the doc. 'Those are just contractions.'"]
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Mixed Messages
+## What is this program?
+**Mixed messages will do the following:**
+1. When this program runs it will produce a new line of text.
+2. this line of text can be one of the following:
+    - A joke
+    - An inspirational message
+    - The word of the day
